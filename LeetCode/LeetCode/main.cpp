fda56f800ab29c15ab4d256f12e4093c965b113d
13891932799 <1257937231@qq.com>
2020-11-24 18:23:17 +0800

    初次尝试

---
+++ LeetCode/LeetCode/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LeetCode
+//
+//  Created by Yuzhe Meng on 2020/11/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
